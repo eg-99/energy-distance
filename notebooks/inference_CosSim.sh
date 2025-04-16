f192448b730eb7edbcda2fb51c9fb97fa9b569ef
eli <14230986+eg-99@users.noreply.github.com>
2025-04-16 17:06:16 -0400

    Update inference_CosSim.sh

--- notebooks/inference_CosSim.sh
+++ notebooks/inference_CosSim.sh
@@ -3,14 +3,12 @@
 #SBATCH --account=edu           # The account name for the job.
 #SBATCH --job-name=mteb-eval_hotpotqa          # The job name.
 #SBATCH -c 2                    # The number of cpu cores to use.
-#SBATCH --time=48:00:00            # The time the job will take to run
-#SBATCH --mem-per-cpu=1gb	# The memory the job will use per cpu core.
+#SBATCH --time=0-48:00:00            # The time the job will take to run
+#SBATCH --mem-per-cpu=5G	# The memory the job will use per cpu core.
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # MODIFY THE FOLLOWING FOR DIFFERENT CONFIGURATIONS
-#SBATCH --nodes=1               # Number of nodes
-#SBATCH --gres=gpu:1            # Number of GPUs per node
-#SBATCH --ntasks-per-node=1     # Number of processes per node
+#SBATCH -N 1               # Number of nodes
 
 module load anaconda
 module load cuda92/toolkit
